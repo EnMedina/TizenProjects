53ab32b694f04111c8ce103278cc62cffafb08ff
Enrique Medina <medina9304@gmail.com>
2019-11-09 19:05:45 -0500

    Build Mindfulness API UI

---
+++ MindfulnesController/js/interval.js
@@ -0,0 +1,8 @@
+// locate your element and add the Click Event Listener
+document.getElementById("parent-list").addEventListener("click",function(e) {
+	// e.target is our targetted element.
+    console.log(e.target.nodeName);
+	if(e.target && e.target.nodeName === "LI") {
+		alert("Setting vibration for " + e.target.id + " minutes");
+	}
+});
---
+++ MindfulnesController/js/vibrationHandler.js
@@ -0,0 +1,5 @@
+basicVibration(); 
+
+function basicVibration() { // Vibrate for 3 seconds 
+	navigator.vibrate(3000); 
+} 
